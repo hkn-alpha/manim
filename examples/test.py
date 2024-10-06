0b00d625670fbb4a71b7106dd0e62913138fb304
Siddharth Kadari <kadaris@gmail.com>
2024-10-06 13:43:58 -0500

    Some components added, terminal connection animation added

--- examples/test.py
+++ examples/test.py
@@ -1,6 +1,7 @@
 from manim import *
-from manim_hkn.cElements import Resistor, Capacitor
+from manim_hkn.cElements import Resistor, Capacitor, BJT_NPN
 from manim_hkn.circuit import Circuit
+from manim_hkn.util import get_terminal_connect_animation
 
 class test(Scene):
 	def construct(self):
@@ -9,17 +10,27 @@ class test(Scene):
 
 		r = Resistor()
 		c = Capacitor()
+		bjt_npn = BJT_NPN()
 
-		self.wait(0.2)
+		self.wait(0.1)
+
+		self.play(Create(bjt_npn))
+
+		self.play(bjt_npn.animate.scale(0.6).shift(3 * DOWN + LEFT))
 
 		self.play(Create(r))
 		self.play(r.animate.scale(0.6))
-		self.play(r.animate.shift(0.6*LEFT*Resistor.SPREAD_RATIO*2+UP))
+		self.play(r.animate.shift(3 * LEFT + 2 * UP))
 
 		self.wait()
 
 		self.play(Create(c))
-		self.play(c.animate.scale(0.6))
-		self.play(c.animate.shift(0.6*1.5*RIGHT+UP))
+		self.play(c.animate.scale(0.6).rotate(-PI/2))
+
+		a = get_terminal_connect_animation(c, c.get_left_terminal_coordinates(), r.get_right_terminal_coordinates())
+		self.play(a)
+
+		a = get_terminal_connect_animation(bjt_npn, bjt_npn.get_gate_terminal_coordinates(), c.get_right_terminal_coordinates())
+		self.play(a)
 
 		self.wait()
--- manim_hkn/__init__.py
+++ manim_hkn/__init__.py
@@ -1,4 +1,5 @@
 from manim import *
 
-from manim_hkn.cElements import Resistor
+from manim_hkn.cElements import Resistor, Capacitor, BJT_NPN
 from manim_hkn.circuit import Circuit
+from manim_hkn.util import get_terminal_connect_animation
