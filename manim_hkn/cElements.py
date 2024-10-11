b7c47390f80d8a91f581145707150ec6fb2e028d
Siddharth Kadari <kadaris@gmail.com>
2024-10-11 00:44:15 -0500

    template parameter cleanup

--- manim_hkn/cElements.py
+++ manim_hkn/cElements.py
@@ -2,14 +2,24 @@ from manim import *
 from enum import Enum
 
 class CircuitElementTemplate(VMobject):
-	def __init__(self, components: List[VMobject], terminalCoords: List[List[float]], stroke_width = 15, **kwargs):
+	def __init__(self, components: List[VMobject], terminalCoords: List[List[float]], **kwargs):
+		kwargs['stroke_width'] 	= kwargs.get('stroke_width', 15)
+		kwargs['color'] 		= kwargs.get('color', WHITE)
+		kwargs['joint_type'] 	= kwargs.get('joint_type', LineJointType.ROUND)
+		kwargs['cap_style'] 	= kwargs.get('cap_style', CapStyleType.ROUND)	
+		VMobject.__init__(self,	**kwargs)
+		
 		self._terminal_scale_factor = 0.5
-		VMobject.__init__(self, stroke_width=stroke_width, joint_type = LineJointType.ROUND, cap_style = CapStyleType.ROUND, **kwargs)
 
-		self._terminals = [Dot(radius=self._terminal_scale_factor * stroke_width/200., color=self.color).shift(coord) for coord in terminalCoords]
+		self._terminals = [Dot(radius=self._terminal_scale_factor * self.stroke_width/200., color=self.color).shift(coord) for coord in terminalCoords]
 		self._components = components
+		for component in self._components:
+			component.stroke_width = self.stroke_width
+			component.joint_type = self.joint_type
+			component.cap_style = self.cap_style
+			component.set_color(self.color)
 
-		self.add(*self._components, *self._terminals).set_color(self.color)
+		self.add(*self._components, *self._terminals)
 
 		def updater(cElem: CircuitElementTemplate):
 			cElem.set_stroke_width(100*cElem._terminals[0].width / self._terminal_scale_factor)
@@ -30,14 +40,15 @@ class BJT_NPN(CircuitElementTemplate):
 		RIGHT 	= 1
 		GATE 	= 2
 
-	def __init__(self, stroke_width=15, **kwargs):
-		circle = Circle(radius = stroke_width / 8., stroke_width = stroke_width)
+	def __init__(self, **kwargs):
+		stroke_width = kwargs.get('stroke_width',15)
 
+		circle = Circle(radius = stroke_width / 8., stroke_width = stroke_width)
 		corners = [	[3, -1, 0], [1.3, -1, 0], [0.8, 0.8, 0], 
 					[1.2, 0.8, 0], [0, 0.8, 0], [0, 2.3, 0], [0, 0.8, 0], [-1.2, 0.8, 0],
 					[-0.8, 0.8, 0], [-1.3, -1, 0], [-3, -1, 0]]
-		component = VMobject(stroke_width=stroke_width, joint_type = LineJointType.ROUND, cap_style = CapStyleType.ROUND).set_points_as_corners(corners)
-		arrow = Line(start = [-0.8, 0.8, 0], end = [(-1.3 - -0.8) * BJT_NPN.ARROW_DIST_RATIO + -0.8, (-1 - 0.8) * BJT_NPN.ARROW_DIST_RATIO + 0.8, 0], buff = 0, stroke_width = stroke_width)
+		component = VMobject().set_points_as_corners(corners)
+		arrow = Line(start = [-0.8, 0.8, 0], end = [(-1.3 - -0.8) * BJT_NPN.ARROW_DIST_RATIO + -0.8, (-1 - 0.8) * BJT_NPN.ARROW_DIST_RATIO + 0.8, 0], buff = 0)
 		arrow.add_tip(tip_shape=ArrowTriangleFilledTip, tip_width = stroke_width / 100. * 4, tip_length = stroke_width / 100. * 4)
 
 		CircuitElementTemplate.__init__(self, 
@@ -54,13 +65,13 @@ class Capacitor(CircuitElementTemplate):
 		LEFT 	= 0
 		RIGHT 	= 1
 
-	def __init__(self, stroke_width=15, color=WHITE, **kwargs):
+	def __init__(self, **kwargs):
 
 		cornersLeft  = [[-1.5, 0, 0], [-0.5, 0, 0], [-0.5, Capacitor.HEIGHT_RATIO, 0], [-0.5, -Capacitor.HEIGHT_RATIO, 0]]
 		cornersRight = [[ 1.5, 0, 0], [ 0.5, 0, 0], [ 0.5, Capacitor.HEIGHT_RATIO, 0], [ 0.5, -Capacitor.HEIGHT_RATIO, 0]]
 		
-		left  = VMobject(stroke_width=stroke_width, color=color, joint_type = LineJointType.ROUND, cap_style = CapStyleType.ROUND).set_points_as_corners(cornersLeft)
-		right = VMobject(stroke_width=stroke_width, color=color, joint_type = LineJointType.ROUND, cap_style = CapStyleType.ROUND).set_points_as_corners(cornersRight)
+		left  = VMobject().set_points_as_corners(cornersLeft)
+		right = VMobject().set_points_as_corners(cornersRight)
 		
 		CircuitElementTemplate.__init__(self, 
 								  components=[left, right], 
@@ -75,7 +86,7 @@ class Resistor(CircuitElementTemplate):
 		LEFT 	= 0
 		RIGHT 	= 1
 
-	def __init__(self, stroke_width=15, color=WHITE, **kwargs):
+	def __init__(self, **kwargs):
 		corners = [[Resistor.SPREAD_RATIO*(-2),  0, 0]]
 		for i in range(-1, 1+1, 1):
 			corners.extend([[Resistor.SPREAD_RATIO*(i-0.5),  0, 0],
@@ -84,7 +95,7 @@ class Resistor(CircuitElementTemplate):
 							[Resistor.SPREAD_RATIO*(i+0.5),  0, 0]])
 		corners.append([Resistor.SPREAD_RATIO*(2),  0, 0])
 		
-		component = VMobject(stroke_width=stroke_width, color=color, joint_type = LineJointType.ROUND, cap_style = CapStyleType.ROUND).set_points_as_corners(corners)
+		component = VMobject().set_points_as_corners(corners)
 
 		CircuitElementTemplate.__init__(self, 
 								  components=[component], 
