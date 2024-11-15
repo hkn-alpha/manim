c090aa5bcdfcc9245142b36703582e98bfe45c48
Siddharth Kadari <kadaris@gmail.com>
2024-11-14 23:10:23 -0600

    Added Function Generator

--- manim_hkn/__init__.py
+++ manim_hkn/__init__.py
@@ -1,6 +1,6 @@
 from manim import *
 
-from manim_hkn.cElements import Resistor, Capacitor, Inductor, BJT_NPN, Wire, Battery, FunctionGenerator, OpAmp, Ground
+from manim_hkn.cElements import Resistor, Capacitor, Inductor, BJT_NPN, Wire, Battery, FunctionGenerator, FunctionGenerator, OpAmp, Ground
 from manim_hkn.utils.circuitBuilder import connect_with_straight_wire, connect_with_square_wire, split_wire
 
 __all__ = [
