025917a8bdc3c09c69da5392a74c7340ce6ceea2
karoljk2 <143351711+karoljk2@users.noreply.github.com>
2025-04-06 14:41:57 -0500

    Update __init__.py

--- manim_hkn/__init__.py
+++ manim_hkn/__init__.py
@@ -1,9 +1,10 @@
 from manim import *
 
-from manim_hkn.cElements import Resistor, Capacitor, Inductor, BJT_NPN, Wire, Battery, FunctionGenerator, FunctionGenerator, OpAmp, Ground
+from manim_hkn.cElements import Resistor, Capacitor, Inductor, BJT_NPN, Wire, Battery, FunctionGenerator, FunctionGenerator, OpAmp, Ground, CurrentSource
 from manim_hkn.utils.circuitBuilder import connect_with_straight_wire, connect_with_square_wire, split_wire
 
 __all__ = [
+	'CurrentSource'
 	'OpAmp',
 	'BJT_NPN',
 	'Resistor',
